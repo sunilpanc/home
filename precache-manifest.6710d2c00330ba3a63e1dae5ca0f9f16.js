54e8bfd0ae856eb310277860b63d45b5502d45a2
Sunil Panchal <sunilpanchal123.sp@gmail.com>
2020-12-25 22:33:00 +0530

    Updates

---
+++ precache-manifest.6710d2c00330ba3a63e1dae5ca0f9f16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ac2eae8d3c044347df3abbb1720bc21",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "cfde7ee5d714384de0cb",
+    "url": "/home/static/css/main.3139a8f4.chunk.css"
+  },
+  {
+    "revision": "7a6c5510618d722fb00b",
+    "url": "/home/static/js/2.5c90541a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/home/static/js/2.5c90541a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfde7ee5d714384de0cb",
+    "url": "/home/static/js/main.54ce2228.chunk.js"
+  },
+  {
+    "revision": "b456ce948bff78c9abcd",
+    "url": "/home/static/js/runtime-main.bd294fd8.js"
+  },
+  {
+    "revision": "f75038ef031a0f75c4435d89c1c93983",
+    "url": "/home/static/media/sunil.f75038ef.jpg"
+  }
+]);
